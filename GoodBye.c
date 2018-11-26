c280305d013ba3417779c15855dad624990cf4a4
natthanitcha <nam.natthanitcha@gmail.com>
2018-11-26 11:27:19 +0700

    First copy 3 files

---
+++ GoodBye.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main()
+{ 
+  printf("Goodbye B!!! 1 2 3 4 5\n");
+  printf("Goodbye A!!! 1 2 3 4 5\n");
+}
---
+++ HelloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+void main()
+{ 
+  printf("Hello World B!!! 1 2 3 4 5\n");
+  printf("Hello World A!!! 1 2 3 4 5\n");
+}
